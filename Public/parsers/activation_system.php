37fc4940f650bff4f17a7c8db5092b9c4e8b75b6
Lyubomir <gavadinov@yahoo.com>
2013-08-16 00:45:05 +0300

    activation view behavior change

--- Public/parsers/activation_system.php
+++ Public/parsers/activation_system.php
@@ -4,8 +4,9 @@ if (isset($_GET['name']) && isset($_GET['email']) && isset($_GET['pass'])) {
 	extract($_GET);
 	$success = false;
 	$options = array('conditions' => array('username = ? AND email = ? AND password = ?', $name, $email, $pass));
-	$user = User::all($options)[0];
+	$user = User::all($options);
 	if ($user) {
+		$user = $user[0];
 		$user->activated = '1';
 		$user->save();
 		$success = true;
@@ -20,15 +21,16 @@ if (isset($_GET['name']) && isset($_GET['email']) && isset($_GET['pass'])) {
 		    $(document).ready(function() {
 		        $(".modal").modal("toggle");
 		        setInterval(function() {
-		        location.assign("'.BASE_DIR.'");
-		        }, 5000);
+		        	location.assign("'.BASE_DIR.'");
+		        }, 4000);
 		    });
 		</script>
 	';
 	view('views/activation', array(
 		'header' => 'header.login.php',
     	'local' => $local,
-    	'script_bottom' => $script_b
+    	'script_bottom' => $script_b,
+    	'success' => $success
     	));
 }
 
--- Public/views/activation.view.php
+++ Public/views/activation.view.php
@@ -1,10 +1,20 @@
 <div class="modal hide fade">
     <div class="modal-header">
-        <h3>Succesfull Activation</h3>
+        <?php if ($success): ?>
+            <h3>Succesfull Activation</h3>
+        <?php else: ?>    
+            <h3 class="text-error">There was a problem with your activation</h3>
+        <?php endif ?>
+        
     </div>
     <div class="modal-body">
         <div class="span4 offset1">
-            <h4>You'll be redirected shortly!</h4>
+            <?php if ($success): ?>
+                <h4>You'll be redirected shortly!</h4>
+            <?php else: ?>    
+                <h4>Please contact an administrator.</h4>
+            <?php endif ?>
+            
             
         </div>
     </div>
