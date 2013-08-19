0caa9e2e8c0b35fbb323777e66462cf3f34c3f47
Lyubomir <gavadinov@yahoo.com>
2013-08-20 01:58:04 +0300

    description system and upload photos system ready

---
+++ Public/views/description.php
@@ -0,0 +1,12 @@
+<div class="modal hide fade" id="descriptionModal">
+	<div class="modal-header">
+        <button class="close" data-dismiss="modal">&times;</button>
+        <h3>Description</h3>
+    </div>
+    <div class="modal-body">
+    	<textarea id="descriptionModalInput"><?= $p->description ?></textarea>
+    </div>
+    <div class="modal-footer">
+    	<button class="btn btn-primary-lighten" id="descriptionModalSubmit">Save</button>
+    </div>
+</div>
